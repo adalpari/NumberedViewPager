97a172b9f58169c9906197d695da9c69564c4d62
Adalberto <>
2018-02-16 22:03:17 +0100

    Some refactor

--- app/src/main/java/adalpari/github/com/numberedviewpager/NumberedViewPagerHandler.java
+++ app/src/main/java/adalpari/github/com/numberedviewpager/NumberedViewPagerHandler.java
@@ -8,9 +8,7 @@ import android.animation.AnimatorSet;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
@@ -28,6 +26,9 @@ public class NumberedViewPagerHandler extends RelativeLayout {
 
     private ViewPager viewPager;
     private TextView counter;
+
+    private int numberCounterColor = Color.BLACK;
+    private int numberCounterTextColor = Color.WHITE;
 
     private AnimatorSet numberAnimator;
 
@@ -52,22 +53,25 @@ public class NumberedViewPagerHandler extends RelativeLayout {
     }
 
     private void initViews(Context context, AttributeSet attrs) {
-        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.NumberCounter);
-
-        int numberCounterColor = Color.BLACK;
-        int numberCounterTextColor = Color.WHITE;
-        try {
-            numberCounterColor = a.getColor(R.styleable.NumberCounter_nc_color, Color.BLACK);
-            numberCounterTextColor = a.getColor(R.styleable.NumberCounter_nc_text_color, Color.WHITE);
-        } catch (Throwable throwable) {
-        } finally {
-            a.recycle();
-        }
-
+        initColors(context, attrs);
         initViewPager(context);
-        initCounter(context, numberCounterColor, numberCounterTextColor);
+        initCounter(context);
         initAnimation(context);
         initNumberListener();
+    }
+
+    private void initColors(Context context, AttributeSet attrs) {
+        if (context != null && attrs != null) {
+            TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.NumberCounter);
+
+            try {
+                numberCounterColor = a.getColor(R.styleable.NumberCounter_nc_color, Color.BLACK);
+                numberCounterTextColor = a.getColor(R.styleable.NumberCounter_nc_text_color, Color.WHITE);
+            } catch (Throwable throwable) {
+            } finally {
+                a.recycle();
+            }
+        }
     }
 
     private void initViewPager(Context context) {
@@ -76,36 +80,44 @@ public class NumberedViewPagerHandler extends RelativeLayout {
         this.addView(viewPager);
     }
 
-    private void initCounter(Context context, int numberCounterColor, int numberCounterTextColor) {
+    private void initCounter(Context context) {
+        int paddingInPx = context.getResources().getDimensionPixelOffset(R.dimen.number_counter_padding);
+
+        counter = new TextView(context);
+
+        counter.setPadding(paddingInPx, paddingInPx, paddingInPx, paddingInPx);
+        counter.setGravity(Gravity.RIGHT);
+        counter.setLayoutParams(initLayoutParams(context));
+        counter.setTextSize(TypedValue.COMPLEX_UNIT_SP, 12);
+        counter.setTextColor(numberCounterTextColor);
+
+        GradientDrawable roundedBackground = initCounterBackground();
+        if(android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+            counter.setBackgroundDrawable(roundedBackground);
+        } else {
+            counter.setBackground(roundedBackground);
+        }
+
+        this.addView(counter);
+    }
+
+    private RelativeLayout.LayoutParams initLayoutParams(Context context) {
         final RelativeLayout.LayoutParams layoutParams = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         layoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
 
         int marginInPx = context.getResources().getDimensionPixelOffset(R.dimen.number_counter_margin);
         layoutParams.setMargins(marginInPx, marginInPx, marginInPx, marginInPx);
 
-        counter = new TextView(context);
-        counter.setLayoutParams(layoutParams);
+        return layoutParams;
+    }
 
-        int paddingInPx = context.getResources().getDimensionPixelOffset(R.dimen.number_counter_padding);
-        counter.setPadding(paddingInPx, paddingInPx, paddingInPx, paddingInPx);
-        counter.setGravity(Gravity.RIGHT);
-
+    private GradientDrawable initCounterBackground() {
         GradientDrawable roundedBackground = new GradientDrawable();
         roundedBackground.setShape(GradientDrawable.RECTANGLE);
         roundedBackground.setCornerRadii(new float[] { 20, 20, 20, 20, 20, 20, 20, 20 });
         roundedBackground.setColor(numberCounterColor);
 
-        final int sdk = android.os.Build.VERSION.SDK_INT;
-        if(sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
-            counter.setBackgroundDrawable(roundedBackground);
-        } else {
-            counter.setBackground(roundedBackground);
-        }
-
-        counter.setTextSize(TypedValue.COMPLEX_UNIT_SP, 12);
-        counter.setTextColor(numberCounterTextColor);
-
-        this.addView(counter);
+        return roundedBackground;
     }
 
     private void initAnimation(Context context) {
@@ -115,6 +127,10 @@ public class NumberedViewPagerHandler extends RelativeLayout {
     }
 
     private void initNumberListener() {
+        if (viewPager == null) {
+            return;
+        }
+
         viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
@@ -140,9 +156,9 @@ public class NumberedViewPagerHandler extends RelativeLayout {
     }
 
     private void setImageNumber(int position) {
-        int total = viewPager.getAdapter().getCount();
-        int realPosition = position + 1;
-        if (counter != null) {
+        if (viewPager != null && counter != null) {
+            int total = viewPager.getAdapter().getCount();
+            int realPosition = position + 1;
             counter.setText(realPosition + "/" + total);
         }
     }
